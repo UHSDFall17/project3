2d70d182a8579701e3aa7fa33064c0bc98b46a49
dhtang <dhtang@localhost>
2017-09-25 12:17:04 -0500

    Updates for User Creation, and Customer Added username variable temporarily Reversioned User Creation to work with Customer

--- src/main/java/edu/cosc4353/group3/AcornsEmulator/Customer.java
+++ src/main/java/edu/cosc4353/group3/AcornsEmulator/Customer.java
@@ -1,17 +1,19 @@
 package edu.cosc4353.group3.AcornsEmulator;
 
-public class Customer {
-	
+// First Implementation of a Customer object
+// TODO Remove user variable and create hash instead
+// TODO Method to compare passwords after matching with obj in hashtable
+public class Customer{
+	private String user;
 	private String passwd;
 	private int card;
-	
-	Customer(String password, int cardNumber){
+
+	public Customer(String username, String password, int cardNumber) {
+		user = username;
 		passwd = password;
 		card = cardNumber;
 	}
-	public String getPassword() {
-		return passwd;
-	}
+	
 	public int getCardNumber() {
 		return card;
 	}
--- src/main/java/edu/cosc4353/group3/AcornsEmulator/UserCreation.java
+++ src/main/java/edu/cosc4353/group3/AcornsEmulator/UserCreation.java
@@ -1,22 +1,13 @@
 package edu.cosc4353.group3.AcornsEmulator;
 
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Scanner;
 
-public class AppTest
-{
-	// Test for Customer class
-    @Test
-    public void testAddCustomer() {
-    	Customer customer = new Customer("asdf","1234",1234);
-    	assertEquals(1234, customer.getCardNumber());
-    }
-    // Test for Create User
-    @Test
-    public void testAddUser() {
-    	Scanner input = new Scanner(System.in);
+// Reversion of smeet's code
+public class UserCreation {
+	public static void createUser() {
+		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Enter User: ");
 		String username = input.next();
@@ -29,10 +20,5 @@ public class AppTest
 		
 		Customer customer = new Customer(username, password, cardNumber);
 		assertEquals(cardNumber, customer.getCardNumber());
-    }
-    // TODO make test for calling UserCreation
-    //@Test
-    //public void Test() {
-    //	UserCreation.createUser();
-    //}
+	}
 }
