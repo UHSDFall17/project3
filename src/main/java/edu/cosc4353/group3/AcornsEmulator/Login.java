9ba31e6a6695d0fd9439205f4f180e519698288d
dhtang <dhtang@localhost>
2017-09-26 14:49:45 -0500

    Rohith and Deetrich code added into sorted directories for organization and distribution purposes

---
+++ src/main/java/edu/cosc4353/group3/AcornsEmulator/Login.java
@@ -0,0 +1,27 @@
+package edu.cosc4353.group3.AcornsEmulator;
+
+import java.util.Scanner;
+
+public class Login {
+
+	
+	public static void DisplayLogin(){
+		String UsernameInput;
+		String PasswordInput;
+		System.out.println("Enter Usermame: " );
+		Scanner userName = new Scanner(System.in);
+		UsernameInput = userName.nextLine();
+	    System.out.println("Enter Password: " );
+	    Scanner password = new Scanner(System.in);
+	    PasswordInput = password.nextLine();
+
+	    
+	}
+	
+	
+	public static void main(String[] args){
+		
+		DisplayLogin();
+		
+	}
+}
