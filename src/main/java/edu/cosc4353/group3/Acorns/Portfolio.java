2a8b85a7a78a7efd588a3ba9bdb440e5120fb38c
Rohith Cherian <rohithcherian@gmail.com>
2017-10-20 00:15:41 -0500

    Update Portfolio.java
    Adding Portfolio Page for User account.

--- src/main/java/edu/cosc4353/group3/Acorns/Portfolio.java
+++ src/main/java/edu/cosc4353/group3/Acorns/Portfolio.java
@@ -1,3 +1,4 @@
+/*
 package edu.cosc4353.group3.Acorns;
 
 public class Portfolio {
@@ -14,4 +15,69 @@ public class Portfolio {
 	public double returnPortfolioGrowth() {
 		return growth;
 	}
+}
+*/
+
+import java.util.Scanner;
+
+
+public class Portfolio {
+	
+	
+	
+	 public static void main(String[] args){
+		 System.out.println(";Select an Investment Porfolio Type");
+		 System.out.println("1. Health");
+		 System.out.println("2. Technology");
+		 System.out.println("3. Sports");
+		 System.out.println("4. Energy");
+		 System.out.println("5. Business");
+		 System.out.println("6. Music");
+		 System.out.println("7. Art");
+
+
+		 Scanner inputOptionPortfolio = new Scanner(System.in);
+		 int portfolioNumber;
+		 portfolioNumber = inputOptionPortfolio.nextInt();
+		 
+		 switch (portfolioNumber){
+		 
+		 case 1:
+		 System.out.println("Health portfolio added!");
+		 break;
+		 
+		 case 2:
+		 System.out.println("Technology portfolio added!");
+	     break;
+ 
+		 case 3:
+		 System.out.println("Sports portfolio added!");
+
+	     break;
+	     
+		 case 4:
+		 System.out.println("Energy portfolio added!");
+	     break;
+
+		 case 5:
+			 System.out.println("Business portfolio added!");
+		     break;
+
+		 case 6:
+			 System.out.println("Music portfolio added!");
+		     break;
+
+		 case 7:
+			 System.out.println("Art portfolio added!");
+		     break;
+
+		 
+		 }
+		 
+
+
+
+
+
+	  }
 }
