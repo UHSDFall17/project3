0864dc8c8c7ec3a9fb99f931f6fb644ea5e7dfd4
Rohith Cherian <rohithcherian@gmail.com>
2017-11-15 03:10:43 -0600

    update appstart.java

--- src/main/java/edu/cosc4353/group3/Acorns/AppStart.java
+++ src/main/java/edu/cosc4353/group3/Acorns/AppStart.java
@@ -1,7 +1,14 @@
 package edu.cosc4353.group3.Acorns;
 
 public class AppStart {
+	
+	
 	public static void main(String args[]) {
-		StartMenu.UserInput();
+		
+		StartMenu.DisplayMenu();
+		//test
 	}
+	
+	
+	
 }
