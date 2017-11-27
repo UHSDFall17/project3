fd8ac9a85403e8ac6e08278817c4fcae6c8a5cad
dee426 <31871558+dee426@users.noreply.github.com>
2017-11-27 00:38:51 -0600

    Customer Update

--- src/main/java/edu/cosc4353/group3/Acorns/Customer.java
+++ src/main/java/edu/cosc4353/group3/Acorns/Customer.java
@@ -14,26 +14,34 @@ public class Customer {
     private String user;
 	private String passwd;
 	private String card;
-	private String AccountTypeCOrP;
 	private double accountBalance;
 	private int portIdent;
+	private String AccountTypeCOrP;
 
 	private char[] ports;
     private String accountChoice;
-
+    
+    private boolean isInvested;
+    private double[] stockData, bondData;
+    private int InvArray_Length;
 
         File file = new File("");
         public static final int PASS_LENGTH = 6;
 
-	public Customer(String username, String password, String cardNumber,int portIndent, double balance, String AccountType) {
+	public Customer(String username, String password, String cardNumber,int portIndent, double balance, String AccountType, boolean isInvest,
+					double[] stock, double[] bond, int SD_Length) {
 		user = username;
 		passwd = password;
 		card = cardNumber;
 		accountBalance = balance;
 		portIdent = portIndent;
 		AccountTypeCOrP = AccountType;
-
-
+		
+		isInvested = isInvest;
+		stockData = stock;
+		bondData = bond;
+		InvArray_Length = SD_Length;
+		
 		ports = new char[4];
 	}
 	
@@ -85,6 +93,41 @@ public class Customer {
             accountBalance =  accountBalance + change;
         }
         
+        
+        public void setInvestData(double[] data, int dataType)  // 0 FOR BOND    1 FOR STOCK
+        {
+        	if (dataType == 0){	bondData = data;	}
+        	else {	stockData = data;	}
+        	
+        }
+        public double[] getInvestData(int dataType)  // 0 FOR BOND    1 FOR STOCK
+        {
+        	if (dataType == 0){	return bondData;	}
+        	else {	return stockData;	}
+        	
+        }
+        
+        public void setStatus_Invest(boolean status)
+        {
+        	isInvested = status;
+        }
+        public boolean getStatus_Invest()
+        {
+        	return isInvested;
+        }
+        
+        
+        public void InvestArray_Length(int length)
+        {
+        	InvArray_Length = length;
+        }
+        public int getInvestArray_Length()
+        {
+        	return InvArray_Length;
+        }
+        
+        
+        
         public void setCardNumber(Customer c ,String number)
         {
             c.card = number;
@@ -118,30 +161,12 @@ public class Customer {
 		
 	}
 	
-	// TODO Pull user object if pass match
-	//public static Customer createUser() {
-		//Scanner input = new Scanner(System.in);
-		
-		//System.out.println("Enter User: ");
-		//String username = input.next();
-		
-		//System.out.println("Enter Password: ");
-		//String password = input.next();
-		
-		//System.out.println("Enter Credit Card Number");
-		//int cardNumber = input.nextInt();
-		
-		//Customer customer = new Customer(username, password, cardNumber);
-		//input.close();
-	//	return customer;
-//	}
-        
 
+        
         
         //Create New User Account
 
         
-
         public static boolean CreateAccountPersonal (String accountChoice) {
         	
         	Scanner input = new Scanner(System.in);
@@ -195,11 +220,9 @@ public class Customer {
 
             return true;
         }
-
         
         
         public static boolean CreateAccountCorporate ( String accountChoice) {
-
 	 		Scanner input = new Scanner(System.in);
         	System.out.println("User Creation! ");
             String username = "";
@@ -252,13 +275,10 @@ public class Customer {
             return true;
         }
         
-        
         //Store Accepted User Login Data in .txt
         public static void StoreUserData(String user, String pass, String cardNumb, String accountChoice)
         {
-
             String Storage = user + " " + pass + " " + cardNumb + " " + "-1" + " "+ "0" + " " + accountChoice; //user0 pass1 cardNo2 portfolio3 accountStatus4
-
             System.out.println(Storage);
             
             try
@@ -337,6 +357,9 @@ public class Customer {
         //Check User Existance in .txt
         public static boolean CheckLogin(String user, String pass) //Check if User login exists and is correct
         {
+        	double[] InvestStats = new double[1];
+        	boolean isInvestor = false;
+        	
             File file = new File("LoginInfo.txt");
             try 
         {    
@@ -356,13 +379,17 @@ public class Customer {
                     double accBal = Double.parseDouble(passString[4]);
                     String accountType = passString[5];
 
-                    Customer CurrentCustomer = new Customer(passString[0],passString[1], cardNo, portId, accBal, accountType);
+                    
+                    Customer CurrentCustomer = new Customer(passString[0],passString[1], cardNo, portId, accBal, accountType,isInvestor, InvestStats, InvestStats, -1);
+                    
+                    InvestmentHandler.CheckAccount(CurrentCustomer);
+                    
+                    	
+                    
+                    
 
-                   // System.out.println("User: " + CurrentCustomer.getUser_Name());
-                   // System.out.println("Pass: " + CurrentCustomer.getUser_Pass());
-                  //  System.out.println("Card: " + CurrentCustomer.getUser_CardNumb());
-                  //  System.out.println("Porti: " + CurrentCustomer.getUser_PortId());
-                  //  System.out.println("Balance: " + CurrentCustomer.getUser_balance());
+
+    
                     UserInterface.UserInput(CurrentCustomer);
                     
                     return true;
@@ -384,6 +411,7 @@ public class Customer {
             return false;
         }
 }
+
 
 
 
