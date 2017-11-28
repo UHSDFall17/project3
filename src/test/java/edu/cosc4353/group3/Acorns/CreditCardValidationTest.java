80e97863596847bf4821eacf346a5e2043ca0d5a
csmeet1495 <31972083+csmeet1495@users.noreply.github.com>
2017-11-28 16:25:12 -0600

    test

---
+++ src/test/java/edu/cosc4353/group3/Acorns/CreditCardValidationTest.java
@@ -0,0 +1,29 @@
+package edu.cosc4353.group3.Acorns;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+*
+* @author Chitalia
+*/
+
+public class CreditCardValidationTest 
+{
+
+	@Test
+	
+	public void test()
+	{
+		double card_no = 4342580102087732L;
+		String card_name = "test";
+		
+		CreditCardValidation test = new CreditCardValidation(card_no, card_name);
+		Assert.assertEquals(card_no, test.returnCCValidatecard_no(), .001);
+		Assert.assertEquals(card_name, test.returnCCValidatecard_name());
+	}
+
+}
