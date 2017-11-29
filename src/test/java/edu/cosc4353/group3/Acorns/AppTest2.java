ef9494510c35d43c3e14b8290a055b143ad1c1e5
csmeet1495 <31972083+csmeet1495@users.noreply.github.com>
2017-11-29 15:55:27 -0600

    User Password and Account Type test case

---
+++ src/test/java/edu/cosc4353/group3/Acorns/AppTest2.java
@@ -0,0 +1,62 @@
+package edu.cosc4353.group3.Acorns;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class AppTest3
+{
+	
+	@Test
+	public void AccountTypetest()
+	{
+		
+			String user = "Smeet";
+			String password = "111111";
+			String cardNo = "4929933965135652";
+			int portIden = 1;
+			double balance = 100;
+			String accountType = "corporate";
+			boolean isInvest = true;
+			double[] stock = new double[10];
+			double[] bond = new double[10];
+			int investLength = 4;
+		
+		Customer customer = new Customer(user, password, cardNo, portIden, balance, accountType
+						,isInvest, stock, bond, investLength);
+		
+		Assert.assertEquals(accountType, customer.getUser_AccountType());
+		
+	}
+	
+	@Test
+	public void Passwordtest()
+	{
+		
+			String user = "Smeet";
+			String password = "111111";
+			String cardNo = "4929933965135652";
+			int portIden = 1;
+			double balance = 100;
+			String accountType = "corporate";
+			boolean isInvest = true;
+			double[] stock = new double[10];
+			double[] bond = new double[10];
+			int investLength = 4;
+		
+		Customer customer = new Customer(user, password, cardNo, portIden, balance, accountType
+						,isInvest, stock, bond, investLength);
+
+		Assert.assertEquals(password, customer.getUser_Pass());
+						
+	}
+	
+
+    public static void main(String[] args) {
+    	
+        // TODO code application logic here
+              StartMenu start = new StartMenu();
+
+              start.DisplayMenu();
+    }
+
+}
