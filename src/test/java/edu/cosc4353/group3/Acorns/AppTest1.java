e9ab7bab349b7affd7f829ef430b12aaa485d8ef
csmeet1495 <31972083+csmeet1495@users.noreply.github.com>
2017-11-29 15:42:48 -0600

    updated with customer username test case

--- src/test/java/edu/cosc4353/group3/Acorns/AppTest1.java
+++ src/test/java/edu/cosc4353/group3/Acorns/AppTest1.java
@@ -26,6 +26,28 @@ public class AppTest1
 					
 	}
 	
+	@Test
+	public void testCardNumb()
+	{
+		
+		String user = "Smeet";
+		String password = "111111";
+		String cardNo = "4929933965135652";
+		int portIden = 1;
+		double balance = 100;
+		String accountType = "corporate";
+		boolean isInvest = true;
+		double[] stock = new double[10];
+		double[] bond = new double[10];
+		int investLength = 4;
+	
+	Customer customer = new Customer(user, password, cardNo, portIden, balance, accountType
+					,isInvest, stock, bond, investLength);
+	
+	Assert.assertEquals(user, customer.getUser_Name());
+					
+	}
+	
 	public static void main(String[] args)
 	{
     	
